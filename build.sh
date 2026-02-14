eae0766831cfe647ad9a3a8cbc28a86d76883b6a
Brett Kuskie <fullaxx@gmail.com>
2026-02-14 09:09:06 +0000

    Pull base images before building

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#!/bin/bash
+
+for BASEIMAGE in `grep '^FROM' Dockerfile.* | awk '{print $2}'`; do
+  docker pull ${BASEIMAGE}
+done
+
 docker build -f Dockerfile.alpine -t ghcr.io/fullaxx/novnc:alpine .
 
 docker build -f Dockerfile.bullseye -t ghcr.io/fullaxx/novnc:bullseye .
@@ -10,4 +16,4 @@ docker build -f Dockerfile.focal -t ghcr.io/fullaxx/novnc:focal .
 docker build -f Dockerfile.jammy -t ghcr.io/fullaxx/novnc:jammy .
 docker build -f Dockerfile.noble -t ghcr.io/fullaxx/novnc:noble .
 
-docker tag ghcr.io/fullaxx/novnc:noble ghcr.io/fullaxx/novnc:latest
+docker tag ghcr.io/fullaxx/novnc:trixie ghcr.io/fullaxx/novnc:latest
